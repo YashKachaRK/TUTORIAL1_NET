7f09afb23cd337930d754e9ac8765730bcdbfc77
YashKachaRK <ykacha243@rku.ac.in>
2025-07-01 15:44:18 +0530

    new

--- Q9.cs
+++ Q9.cs
@@ -8,5 +8,12 @@ namespace TUTORIAL1
 {
     internal class Q9
     {
+        public static void Main(string[] args)
+        {
+            string s1 = "yash kacha";
+            string s2 = s1.();
+            Console.WriteLine("Before:- " + s1);
+            Console.WriteLine("After:- " + s2);
+        }
     }
 }
