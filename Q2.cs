626fc31b11cc8e455ce309eaf4a27b9eb2f17586
YashKachaRK <ykacha243@rku.ac.in>
2025-07-16 08:50:11 +0530

    new code

--- Q2.cs
+++ Q2.cs
@@ -10,14 +10,14 @@ namespace TUTORIAL1
     {
         public static void Main (string[] args)
         {
-            Console.WriteLine("Name : Kacha Yash");
-            Console.WriteLine("DOB: 03/11/2005");
-            Console.WriteLine("Address: AHIR CHOWK , BOL BALA MAIN ROAD");
+            Console.WriteLine("Name : Pandya Gracy");
+            Console.WriteLine("DOB: 19/06/2003");
+            Console.WriteLine("Address: AJI DEM NEAR");
             Console.WriteLine("City : Rajkot");
             Console.WriteLine("Pincode : 360002");
             Console.WriteLine("State: Gujrat");
             Console.WriteLine("Country : India");
-            Console.WriteLine("Email : ykacha243@rku.ac.in");
+            Console.WriteLine("Email : gpandya713@rku.ac.in");
            
         }
     }
--- Q8.cs
+++ Q8.cs
@@ -10,7 +10,7 @@ namespace TUTORIAL1
     {
         public static void Main (string[] args)
         {
-            string s1 = "yash kacha";
+            string s1 = "gracy pandya";
             string s2 = s1.ToUpper();
             Console.WriteLine ("Before:- "+s1);
             Console.WriteLine ("After:- "+s2);
